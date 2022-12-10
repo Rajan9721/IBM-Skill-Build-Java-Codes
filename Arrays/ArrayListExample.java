00f745f5511d4b2d2261f9336635dbb3a1677a0d
Rajan9721 <rn638600@gmail.com>
2022-12-10 09:59:29 +0530

    adding Remove in arraylist

--- Arrays/ArrayListExample.java
+++ Arrays/ArrayListExample.java
@@ -28,6 +28,12 @@ public class ArrayListExample {
         sherlocksToDos.set(1,"listen to Dr. Watson for amusement");
         poirotsToDos.set(3, "listen to Captain Hastings for amusement");
 
+        // Remove each to-do below:
+        sherlocksToDos.remove("visit the crime scene");
+        poirotsToDos.remove(0);
+        sherlocksToDos.remove("play violin");
+
+
         System.out.println("Sherlock's to-do list:");
         System.out.println(sherlocksToDos.toString() + "\n");
         System.out.println("Poirot's to-do list:");
