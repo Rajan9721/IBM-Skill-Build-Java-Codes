bfb83f359ca42122dbfdd7f50e7d10667cd074d8
Rajan9721 <rn638600@gmail.com>
2022-12-17 19:11:07 +0530

    String Methods IBM Skill Builds Programs

---
+++ String-Methods/CombineNames.java
@@ -0,0 +1,13 @@
+public class CombineNames {
+    public static void main(String[] args) {
+
+        // Add your first names:
+        String firstName = "Rajan";
+        String lastName = " Rajbhar";
+
+        // What's the full name?
+
+        System.out.println(firstName.concat(lastName));
+
+    }
+}
